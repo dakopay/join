227c4038b490b89bc064df13a7822ad8e11e84ea
Aman Singh <besoeasy@users.noreply.github.com>
2022-11-17 14:56:22 +0530

    ----

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,3 @@
 <template>
-	<section class="min-h-screen bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400" @click="runtoweb()"></section>
+	<section class="min-h-screen bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400"></section>
 </template>
-
-<script>
-	export default {
-		methods: {
-			runtoweb() {
-				window.location.href = 'https://repa.gg/#/create';
-			},
-		},
-	};
-</script>
