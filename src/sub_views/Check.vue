60edc5f8b8d020430c22e2543e67938f4f254013
besoeasy <besoeasy@users.noreply.github.com>
2022-12-04 04:24:05 +0530

    clean push

--- src/sub_views/Check.vue
+++ src/sub_views/Check.vue
@@ -40,7 +40,7 @@
 
 			this.desktop_url = `https://join.dakopay.com/#/join/${this.hash}`;
 
-			this.trustwallet_url = `https://link.trustwallet.com/open_url?coin_id=60&url=https://join.dakopay.com/#/join/${this.hash}`;
+			this.trustwallet_url = `https://link.trustwallet.com/open_url?url=https://join.dakopay.com/#/join/${this.hash}`;
 
 			this.metamask_url = `https://metamask.app.link/dapp/join.dakopay.com/#/join/${this.hash}`;
 
